0b4a315a5a9cec06d8e2988fe0dee6fedaed2298
Nitesh Kumar <nit.glad@gmail.com>
2026-01-13 11:43:17 +0100

    Initial React app

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div style={{ padding: "2rem", fontfamily: "Ariel" }}>
+      <h1> Hello CI/CD </h1>
+      <p>This react app will be deployed automatically.</p>
+    </div>
+ );
+}
+export default App
