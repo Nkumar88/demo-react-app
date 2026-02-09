ed558e8a74f341f096df366cba49563991d28768
Nitesh Sharma <nitesh.sharma@odido.nl>
2026-02-09 16:10:11 +0100

    new change

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 function App() {
+
+  console.log("RUNTIME CONFIG:", window.RUNTIME_CONFIG);
+  const text = window.RUNTIME_CONFIG?.CUSTOM_HEADER;
   return (
-    <div style={{ padding: "2rem", fontfamily: "Ariel" }}>
+    <div style={{ padding: "2rem", fontfamily: "Arial" }}>
       <h1>Hello from CI/CD 🚀</h1>
       <p>This react app will be deployed automatically.</p>
     </div>
